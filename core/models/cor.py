fe285e42d03ef01d5125087a1efaeb78c2eed16f
vittorms <vitor.msalomao@gmail.vom>
2024-07-09 16:15:29 -0300

    feat: Criação do endpoint Marcas

--- core/models/cor.py
+++ core/models/cor.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Cor(models.Model):
-    descricao = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.descricao} - {self.id}"
+        return f"{self.nome} - {self.id}"
     
     class Meta: 
         verbose_name = "Cor"
