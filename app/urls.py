8be2e83880c38198b36a46a17ad05c3489cfe135
vittorms <vitor.msalomao@gmail.com>
2024-07-09 15:11:50 -0300

    feat: Criação do endpoint Cor

--- app/urls.py
+++ app/urls.py
@@ -9,13 +9,14 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet,AcessorioViewSet,CategoriaViewSet
+from core.views import UserViewSet,AcessorioViewSet,CategoriaViewSet, CorViewSet
 
 router = DefaultRouter()
 
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 router.register(r"acessorios", AcessorioViewSet, basename="acessorios")
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
+router.register(r"cores", CorViewSet, basename="cores")
 urlpatterns = [
     path("admin/", admin.site.urls),
     # OpenAPI 3
